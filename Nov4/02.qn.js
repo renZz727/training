437c240c47fbcd10aba947710266a2b24af202ff
renZz726 <rensonkroji02@gmail.com>
2025-11-04 14:46:39 +0530

    02.qn.js

--- Nov4/02.qn.js
+++ Nov4/02.qn.js
@@ -1,15 +1,17 @@
 function functionCompositionFn(functions, num) {
-  if (functions.length === 0) return num;
-  let value = num;
   let func;
-  for (let i = functions.length - 1; i >= 0; i--) {
-    func = functions[i];
-    value = func(value);
+  function fnComposition(value = num) {
+    if (functions.length === 0) return value;
+    for (let i = functions.length - 1; i >= 0; i--) {
+      func = functions[i];
+      value = func(value);
+    }
+    return value;
   }
-  return value;
+  return fnComposition;
 }
 
-console.log(
-  functionCompositionFn([(x) => x + 1, (x) => x * x, (x) => 2 * x], 4)
-);
-console.log(functionCompositionFn([(x) => 2 * x, (x) => 10, (x) => x - 1], 3))
+const fn = functionCompositionFn([(x) => x + 1, (x) => x * x, (x) => 2 * x], 4);
+console.log(fn()); // 65
+console.log(fn(4)); // 65
+console.log(fn(5)); // 101
