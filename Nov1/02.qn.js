41da002388a0eed5398efb0448c36bf7b5cf9754
renZz726 <rensonkroji02@gmail.com>
2025-11-04 13:43:29 +0530

    completed

---
+++ Nov1/02.qn.js
@@ -0,0 +1,14 @@
+function functionCompositionFn(functions, num) {
+  if (functions.length === 0) return num;
+  let value = num;
+  let func;
+  for (let i = functions.length - 1; i >= 0; i--) {
+    func = functions[i];
+    value = func(value);
+  }
+  return value;
+}
+
+console.log(
+  functionCompositionFn([(x) => x + 1, (x) => x * x, (x) => 2 * x], 4)
+);
